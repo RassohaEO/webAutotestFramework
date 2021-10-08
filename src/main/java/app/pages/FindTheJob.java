c91bfd3c687863525a86e0ae6b55f447908fdd8a
slava <vprytkin@gmail.com>
2021-10-08 18:07:12 +0300

    add FindTheJob

--- src/main/java/app/pages/FindTheJob.java
+++ src/main/java/app/pages/FindTheJob.java
@@ -1,7 +1,22 @@
 package app.pages;
 
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+
 public class FindTheJob extends BasePage{
+
+  //  public SelenideElement chatField = driver.findElement(By.xpath("//div[@class = 'amo-button-holder amo-horisontal']"));
+
+    public SelenideElement chatButton = $(byXpath("//div[@class='amo-button-holder amo-horisontalt']"));
+
     public FindTheJob(String pageUrl) {
         super(pageUrl);
     }
 }
+
