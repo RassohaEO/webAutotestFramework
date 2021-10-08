7696d6105191f46a9c7c89263adcc8dcfed4eae7
AndreyShushakov <89846420+AndreyShushakov@users.noreply.github.com>
2021-10-08 16:28:31 +0200

    added contacts with test

--- src/main/java/app/pages/Contacts.java
+++ src/main/java/app/pages/Contacts.java
@@ -1,7 +1,24 @@
 package app.pages;
 
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
 public class Contacts extends BasePage{
     public Contacts(String pageUrl) {
         super(pageUrl);
     }
+
+    public SelenideElement userName = $(byName("uname"));
+    public SelenideElement userPhone = $(byName("utel"));
+    public SelenideElement userEmail = $(byName("uemail"));
+    public SelenideElement userMessage = $(byName("umessage"));
+    public SelenideElement submitButton = $(byXpath("//button[@type='submit']"));
+
+
+
+
 }
